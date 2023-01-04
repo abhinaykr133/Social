d0da940e4081046ef7c197fb2c2f59c25462aae9
Abhinay Kumar <abhinaykumar01122000@gmail.com>
2023-01-05 00:52:40 +0530

    Third Commit

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -28,10 +28,10 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    !user && res.status(404).json("user not found");
+    !user && res.status(404).json("user not ");
 
     const validPassword = await bcrypt.compare(req.body.password, user.password)
-    !validPassword && res.status(400).json("wrong password")
+    !validPassword && res.status(400).json("wrong ")
 
     res.status(200).json(user)
   } catch (err) {
